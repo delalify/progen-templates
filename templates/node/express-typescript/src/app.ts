a4ea652208a23a8a833025ba7b01cc38730ced88
Enoch Klu <eenoch@delalify.com>
2022-02-11 07:12:21 +0000

    Use middleware and router

--- templates/node/express-typescript/src/app.ts
+++ templates/node/express-typescript/src/app.ts
@@ -3,16 +3,24 @@ import express, {Request, Response} from 'express'
 
 // Import configuration for the server
 import {SERVER_PORT} from './config/server.config' 
+import exampleRouter from './routes/example.route'
 
 // Initialize an Express application
 const app = express()
+
+// Use middleware that sends responses in JSON format
+app.use(express.json())
 
 // Initialize a route handler
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!')
 })
 
+// Use a router to handle all incoming requests to the `/scream` endpoint
+app.use('/scream', exampleRouter)
+
 // Listen for requests at the server port
 app.listen(SERVER_PORT, () => {
-  return console.log(`Server started at http://localhost:${SERVER_PORT}`)
+  console.log(`Server started at http://localhost:${SERVER_PORT}`)
+  return console.log('Try navigating to `/scream`')
 })
