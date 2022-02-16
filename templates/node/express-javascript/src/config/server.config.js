ead14a87f573ece3de8f08b87c82e793b829680e
Enoch Klu <eenoch@delalify.com>
2022-02-16 09:49:10 +0000

    Format code

--- templates/node/express-javascript/src/config/server.config.js
+++ templates/node/express-javascript/src/config/server.config.js
@@ -3,4 +3,4 @@
 // Use a loaded environment variable or a default of `5000` as the port
 const SERVER_PORT = process.env.PORT || 5000
 
-module.exports = {SERVER_PORT}
+module.exports = { SERVER_PORT }
