1fe1d9de3134ce5c810f21daf686e7e89cd4fd65
Enoch Klu <eenoch@delalify.com>
2022-02-16 09:00:09 +0000

    Send status codes

--- templates/node/express-typescript/src/routes/example.route.ts
+++ templates/node/express-typescript/src/routes/example.route.ts
@@ -9,7 +9,9 @@ const exampleRouter = Router()
 
 // Send a message when the default route is queried
 exampleRouter.get( '/', ( req: Request, res: Response ) => {
-	return res.send( 'You have reached the example endpoint. Try navigating to `/Rick`. Bye.' )
+	return res
+		.status( 200 )
+		.send( 'You have reached the example endpoint. Try navigating to `/Rick`. Bye.' )
 } )
 
 // Pass incoming requests to the controller
@@ -17,18 +19,9 @@ exampleRouter.get( '/*', ( req: Request, res: Response ) => {
 	return exampleController( req, res )
 } )
 
-// Handle unknown routes/endpoints
-exampleRouter.get( '*', ( _req, res ) => {
-	return res.send( {
-		is_error: true,
-		status_code: 404,
-		message: 'The requested route does not exist.',
-	} )
-} )
-
 // Handle unsupported HTTP request methods
 exampleRouter.all( '*', ( req, res ) => {
-	return res.send( {
+	return res.status( 405 ).send( {
 		is_error: true,
 		status_code: 405,
 		message: `Method Not Allowed. Received: '${req.method}'`,
