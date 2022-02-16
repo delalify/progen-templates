51d89f444e11be2509346bf87ac65b61cae8d968
Enoch Klu <eenoch@delalify.com>
2022-02-16 09:50:05 +0000

    Handle unknown routes and unsupported methods

--- templates/node/express-javascript/src/routes/example.route.js
+++ templates/node/express-javascript/src/routes/example.route.js
@@ -1,30 +1,38 @@
 // Import Router function from Express
-import { Router } from 'express';
+const { Router } = require( 'express' )
 
 // Import the example controller
-import  exampleController  from '../controllers/example.controller';
+const exampleController = require( '../controllers/example.controller' )
 
 // Create a router
 const exampleRouter = Router()
 
 // Send a message when the default route is queried
-exampleRouter.get('/', (_req, res) => {
-  return res.send('You have reached the example endpoint. Try navigating to `/Rick`. Bye.')
-})
+exampleRouter.get( '/', ( req, res ) => {
+	return res.send( 'You have reached the example endpoint. Try navigating to `/Rick`. Bye.' )
+} )
 
 // Pass incoming requests to the controller
-exampleRouter.get('/*', (req, res) => {
-  return exampleController(req, res)
-})
+exampleRouter.get( '/*', ( req, res ) => {
+	return exampleController( req, res )
+} )
 
 // Handle unknown routes/endpoints
-exampleRouter.get( '*',  ( _req, res )=> {
-	return res.send({is_error: true, status_code: 404, message: 'The requested route does not exist.'})
+exampleRouter.get( '*', ( req, res ) => {
+	return res.status( 404 ).send( {
+		is_error: true,
+		status_code: 404,
+		message: 'The requested route does not exist.',
+	} )
 } )
 
 // Handle unsupported HTTP request methods
-exampleRouter.all( '*',  ( _req, res ) => {
-	return res.send( {is_error: true, status_code: 405, message: `Method Not Allowed. Received: '${req.method}'`} )
+exampleRouter.all( '*', ( req, res ) => {
+	return res.status( 405 ).send( {
+		is_error: true,
+		status_code: 405,
+		message: `Method Not Allowed. Received: '${req.method}'`,
+	} )
 } )
 
-export default exampleRouter
+module.exports = exampleRouter
